2451c2a5b812fadba07b0a782cf0ac5fc7076817
gamemann <gamemann@gflclan.com>
2021-05-06 14:09:02 +0000

    Add switch and if source files.

---
+++ src/if.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 5;
+
+    if (i == 0)
+    {
+        printf("case #0\n");
+    }
+    else if (i == 1)
+    {
+        printf("case #1\n");
+    }
+    else if (i == 2)
+    {
+        printf("case #2\n");
+    }
+    else if (i == 3)
+    {
+        printf("case #3\n");
+    }
+    else if (i == 4)
+    {
+        printf("case #4\n");
+    }
+    else if (i == 5)
+    {
+        printf("case #5\n");
+    }
+
+    return 0;
+}
---
+++ src/switch.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 5;
+
+    switch (i)
+    {
+        case 0:
+            printf("case #0\n");
+
+            break;
+
+        case 1:
+            printf("case #1\n");
+
+            break;
+
+        case 2:
+            printf("case #2\n");
+
+            break;
+
+        case 3:
+            printf("case #3\n");
+
+            break;
+
+        case 4:
+            printf("case #4\n");
+
+            break;
+
+        case 5:
+            printf("case #5\n");
+
+            break;
+    }
+
+    return 0;
+}
