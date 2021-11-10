6740acad44b22cb8fad2df20bde6e4ecb65fa470
gamemann <gamemann@gflclan.com>
2021-11-10 23:32:21 +0000

    Add empty Assembly code.

---
+++ asm/emptyO1.s
@@ -0,0 +1,17 @@
+	.text
+	.file	"empty.c"
+	.globl	main                    # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	xorl	%eax, %eax
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+
+	.ident	"clang version 6.0.0-1ubuntu2 (tags/RELEASE_600/final)"
+	.section	".note.GNU-stack","",@progbits
---
+++ asm/emptyO3_intel.s
@@ -0,0 +1,18 @@
+	.text
+	.intel_syntax noprefix
+	.file	"empty.c"
+	.globl	main                    # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	xor	eax, eax
+	ret
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+
+	.ident	"clang version 6.0.0-1ubuntu2 (tags/RELEASE_600/final)"
+	.section	".note.GNU-stack","",@progbits
