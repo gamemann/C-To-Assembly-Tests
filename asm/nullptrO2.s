5172a828e9b5440e24a22aa63149b196bfac5076
gamemann <gamemann@gflclan.com>
2021-11-11 00:15:59 +0000

    Update nullptr Assembly.

--- asm/nullptrO2.s
+++ asm/nullptrO2.s
@@ -6,12 +6,26 @@
 main:                                   # @main
 	.cfi_startproc
 # %bb.0:
+	pushq	%rax
+	.cfi_def_cfa_offset 16
+	movq	stdout(%rip), %rdi
+	movl	$.L.str, %esi
+	xorl	%edx, %edx
 	xorl	%eax, %eax
+	callq	fprintf
+	xorl	%eax, %eax
+	popq	%rcx
 	retq
 .Lfunc_end0:
 	.size	main, .Lfunc_end0-main
 	.cfi_endproc
                                         # -- End function
+	.type	.L.str,@object          # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"%d is i.\n"
+	.size	.L.str, 10
+
 
 	.ident	"clang version 6.0.0-1ubuntu2 (tags/RELEASE_600/final)"
 	.section	".note.GNU-stack","",@progbits
--- asm/nullptrO2_intel.s
+++ asm/nullptrO2_intel.s
@@ -7,12 +7,26 @@
 main:                                   # @main
 	.cfi_startproc
 # %bb.0:
+	push	rax
+	.cfi_def_cfa_offset 16
+	mov	rdi, qword ptr [rip + stdout]
+	mov	esi, offset .L.str
+	xor	edx, edx
 	xor	eax, eax
+	call	fprintf
+	xor	eax, eax
+	pop	rcx
 	ret
 .Lfunc_end0:
 	.size	main, .Lfunc_end0-main
 	.cfi_endproc
                                         # -- End function
+	.type	.L.str,@object          # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"%d is i.\n"
+	.size	.L.str, 10
+
 
 	.ident	"clang version 6.0.0-1ubuntu2 (tags/RELEASE_600/final)"
 	.section	".note.GNU-stack","",@progbits
