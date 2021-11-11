2545d0c92d5c983a2422a67eef2d4e4fdec24bcb
gamemann <gamemann@gflclan.com>
2021-11-11 00:16:28 +0000

    Update pointer Assembly.

--- asm/pointerO2_intel.s
+++ asm/pointerO2_intel.s
@@ -7,12 +7,27 @@
 main:                                   # @main
 	.cfi_startproc
 # %bb.0:
+	push	rax
+	.cfi_def_cfa_offset 16
+	mov	rdi, qword ptr [rip + stdout]
+	mov	esi, offset .L.str
+	mov	edx, 1
+	mov	ecx, 1
 	xor	eax, eax
+	call	fprintf
+	xor	eax, eax
+	pop	rcx
 	ret
 .Lfunc_end0:
 	.size	main, .Lfunc_end0-main
 	.cfi_endproc
                                         # -- End function
+	.type	.L.str,@object          # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"i = %d. x = %d.\n"
+	.size	.L.str, 17
+
 
 	.ident	"clang version 6.0.0-1ubuntu2 (tags/RELEASE_600/final)"
 	.section	".note.GNU-stack","",@progbits
