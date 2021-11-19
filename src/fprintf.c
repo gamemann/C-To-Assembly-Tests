f3d29f01e486d16fe7507e8a3e7f361145600a2d
gamemann <gamemann@gflclan.com>
2021-11-19 04:55:26 +0000

    Add printf and fprintf source code.

---
+++ src/fprintf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    fprintf(stdout, "This is a test.\n");
+
+    return 0;
+}
---
+++ src/printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("This is a test.\n");
+
+    return 0;
+}
