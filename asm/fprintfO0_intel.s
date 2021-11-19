5ea641a48b4f1dfec7d77571483fbb497a86a10d
gamemann <gamemann@gflclan.com>
2021-11-19 04:55:55 +0000

    Add new Assembly code for printf and fprintf.

---
+++ asm/fprintfO0_intel.s
@@ -0,0 +1,39 @@
+	.text
+	.intel_syntax noprefix
+	.file	"fprintf.c"
+	.globl	main                    # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register rbp
+	sub	rsp, 16
+	movabs	rsi, offset .L.str
+	mov	dword ptr [rbp - 4], 0
+	mov	rdi, qword ptr [stdout]
+	mov	al, 0
+	call	fprintf
+	xor	ecx, ecx
+	mov	dword ptr [rbp - 8], eax # 4-byte Spill
+	mov	eax, ecx
+	add	rsp, 16
+	pop	rbp
+	ret
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object          # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"This is a test.\n"
+	.size	.L.str, 17
+
+
+	.ident	"clang version 6.0.0-1ubuntu2 (tags/RELEASE_600/final)"
+	.section	".note.GNU-stack","",@progbits
---
+++ asm/printfO0_intel.s
@@ -0,0 +1,38 @@
+	.text
+	.intel_syntax noprefix
+	.file	"printf.c"
+	.globl	main                    # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register rbp
+	sub	rsp, 16
+	movabs	rdi, offset .L.str
+	mov	dword ptr [rbp - 4], 0
+	mov	al, 0
+	call	printf
+	xor	ecx, ecx
+	mov	dword ptr [rbp - 8], eax # 4-byte Spill
+	mov	eax, ecx
+	add	rsp, 16
+	pop	rbp
+	ret
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object          # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"This is a test.\n"
+	.size	.L.str, 17
+
+
+	.ident	"clang version 6.0.0-1ubuntu2 (tags/RELEASE_600/final)"
+	.section	".note.GNU-stack","",@progbits
