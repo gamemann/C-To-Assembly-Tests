4f12c3fd2cb37dbd7afcdc2dbcc85ce9b79ad5d6
gamemann <gamemann@gflclan.com>
2021-11-19 15:38:51 +0000

    Modify Bash script to generate individual Clang and GCC builds.

--- scripts/genassembly.sh
+++ scripts/genassembly.sh
@@ -2,18 +2,22 @@
 ROOTDIR=../
 FILE=$1
 
-# Build with Clang under with no optimizations because LLC will be doing those.
-clang -c -emit-llvm -O0 -o $ROOTDIR/asm/$FILE.bc $ROOTDIR/src/$FILE.c
+# Build with Clang under no optimizations for LLC build.
+clang -c -emit-llvm -O0 -o $ROOTDIR/asm/clangandllc/$FILE.ll $ROOTDIR/src/$FILE.c
 
 # Loop through 0 - 3 optimizations.
 x=0
 while [ $x -lt 4 ]
 do
-    # Non-Intel.
-    llc -filetype=asm -O=$x -o $ROOTDIR/asm/${FILE}O${x}.s $ROOTDIR/asm/$FILE.bc
+    # LLC (Intel and non-Intel).
+    llc -filetype=asm -O=$x -o $ROOTDIR/asm/clangandllc/${FILE}O${x}_intel.s --x86-asm-syntax=intel $ROOTDIR/asm/clangandllc/$FILE.ll
+    llc -filetype=asm -O=$x -o $ROOTDIR/asm/clangandllc/${FILE}O${x}.s $ROOTDIR/asm/clangandllc/$FILE.ll
 
-    # Intel.
-    llc -filetype=asm -O=$x -o $ROOTDIR/asm/${FILE}O${x}_intel.s --x86-asm-syntax=intel $ROOTDIR/asm/$FILE.bc
+    # Clang build.
+    clang -S -O${x} -o $ROOTDIR/asm/clang/${FILE}O${x}.s $ROOTDIR/src/$FILE.c
+
+    # GCC build.
+    gcc -S -O${x} -o $ROOTDIR/asm/gcc/${FILE}O${x}.s $ROOTDIR/src/$FILE.c
 
     # Increment.
     x=$(( $x + 1 ))
